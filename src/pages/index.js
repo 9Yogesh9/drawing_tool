5715549584d56ff9513759faf59507296b84186b
Yogesh Bhat <yogesh99bhat@gmail.com>
2023-10-02 19:15:19 +0530

    Toolbox config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import Menu from '../components/Menu/index';
+import ToolBox from '../components/Toolbox/index';
 
 export default function Home() {
   return (
-    <Menu />
+    <>
+      <Menu />
+      <ToolBox />
+    </>
   )
 }
