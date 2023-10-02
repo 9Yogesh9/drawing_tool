4dfd94d300fefbd5bb1d7fbed30fde1425259407
Yogesh Bhat <yogesh99bhat@gmail.com>
2023-10-02 19:39:49 +0530

    setting canvas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Menu from '../components/Menu/index';
 import ToolBox from '../components/Toolbox/index';
+import Board from '@/components/Board/index';
 
 export default function Home() {
   return (
     <>
       <Menu />
       <ToolBox />
+      <Board />
     </>
   )
 }
