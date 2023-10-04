0ac779c404e301cc942bd7e667c68d492223729b
Yogesh Bhat <yogesh99bhat@gmail.com>
2023-10-04 19:50:35 +0530

    Updated the server URL

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+// https://drawing-tool-server.onrender.com
+const URL = process.env.NODE_ENV === 'production' ? "https://drawing-tool-server.onrender.com" : "http://localhost:5000";
+
+export const socket = io(URL);
